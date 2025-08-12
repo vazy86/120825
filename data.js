fdfb091a25f443242c2e586c2eb28624ba9f04cd
Vazy <35339440+vazy86@users.noreply.github.com>
2025-08-12 17:55:09 +0300

    Add Marzipano panorama project

---
+++ data.js
@@ -0,0 +1,194 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0--",
+      "name": "Главный вход",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -1.908908268549622,
+        "pitch": 0.19119457324762124,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5697751250559442,
+          "pitch": 0.15596996958197806,
+          "rotation": 0.7853981633974483,
+          "target": "3-109_3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.664822686114002,
+          "pitch": 0.03302501265341462,
+          "title": "Info",
+          "text": "Главный вход"
+        }
+      ]
+    },
+    {
+      "id": "1-109_1",
+      "name": "Каб.109_1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 2.8593791737099625,
+        "pitch": 0.21137820687629016,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.507252629728696,
+          "pitch": 0.14349960595267675,
+          "rotation": 0,
+          "target": "2-109_2"
+        },
+        {
+          "yaw": 2.868636566048936,
+          "pitch": 0.11511489309850198,
+          "rotation": 0,
+          "target": "3-109_3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-109_2",
+      "name": "Каб.109_2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -1.125056160688004,
+        "pitch": 0.24624814430650588,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.715069051310004,
+          "pitch": 0.16708914768740968,
+          "rotation": 0,
+          "target": "3-109_3"
+        },
+        {
+          "yaw": -0.8784620434846957,
+          "pitch": 0.14088278734643112,
+          "rotation": 0,
+          "target": "1-109_1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-109_3",
+      "name": "Каб.109_3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.8525410759290608,
+        "pitch": 0.165424197149596,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.305916646566068,
+          "pitch": 0.15413038152222214,
+          "rotation": 5.497787143782138,
+          "target": "0--"
+        },
+        {
+          "yaw": 0.9762532146128127,
+          "pitch": 0.16088901804842592,
+          "rotation": 0,
+          "target": "2-109_2"
+        },
+        {
+          "yaw": 0.3861180502132644,
+          "pitch": 0.11195545217271174,
+          "rotation": 0,
+          "target": "1-109_1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
